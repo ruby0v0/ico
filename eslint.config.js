4532c938b9c30dc75f532ebd887fbf2681b39303
鲨鱼辣椒 <monishine_code@163.com>
2025-08-29 17:06:25 +0800

    chore(config): eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,13 @@ export default antfu({
   formatters: true,
   unocss: true,
   vue: true,
+  ignores: [
+    '**/src/assets/collections.json',
+    '**/public/collections',
+    '**/public/lib',
+    '**/release',
+    '**/collections-info.json',
+    '**/collections-meta.json',
+    '**/dist-electron',
+  ],
 })
