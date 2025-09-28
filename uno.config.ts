27e9dcb4b24c8830c107a547f3f86ec37ddee876
鲨鱼辣椒 <monishine_code@163.com>
2025-09-28 09:46:53 +0800

    chore: 调整uno配置

--- uno.config.ts
+++ uno.config.ts
@@ -8,15 +8,14 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
   shortcuts: {
     'container-wrapper': 'container mx-auto p-4 md:p-5',
     'color-base': 'text-gray-700 dark:text-gray-300',
   },
   presets: [
-    presetWind3(),
-    presetIcons(),
     presetAttributify(),
+    presetIcons(),
+    presetWind3(),
   ],
   transformers: [
     transformerDirectives(),
